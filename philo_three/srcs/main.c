8a214a02dd8eb18436dddc4647ad70aa44acd846
mangupli <gaponova.liz.m@gmail.com>
2021-05-13 13:21:56 +0300

    philo_three and norm

---
+++ philo_three/srcs/main.c
@@ -0,0 +1,78 @@
+#include "philo_three.h"
+
+static void	*phi_life(void *a)
+{
+	long	num;
+
+	num = (long)a;
+	while (1)
+	{
+		if (g_data.must_eat == 0 \
+		|| (g_data.must_eat != 0 && g_data.phil[num].meals < g_data.must_eat))
+		{
+			get_forks(num);
+			g_data.phil[num].meals++;
+			put_down_forks(num);
+			display_message(get_time(), num + 1, SLEEP);
+			usleep(g_data.time_to_sleep * 1000);
+			display_message(get_time(), num + 1, THINK);
+		}
+		else
+			exit (EXIT_SUCCESS);
+	}
+	return (NULL);
+}
+
+static int	create_monitor_threads(void)
+{
+	void	*result;
+
+	if (pthread_create(&g_data.death, NULL, check_death, NULL) == -1)
+		return (ft_error("Can'thread create a thread"));
+	if (g_data.must_eat != 0)
+	{
+		if (pthread_create(&g_data.meal, NULL, finish_meal, NULL) == -1)
+			return (ft_error("Can'thread create a thread"));
+	}
+	if (pthread_join(g_data.death, &result) == -1)
+		return (ft_error("Can'thread join a thread"));
+	if (g_data.must_eat != 0)
+	{
+		if (pthread_join(g_data.meal, &result) == -1)
+			return (ft_error("Can'thread join a thread"));
+	}
+	return (EXIT_SUCCESS);
+}
+
+static int	create_philo_processes(void)
+{
+	long	i;
+	int		status;
+	int		ret;
+
+	i = -1;
+	ret = EXIT_SUCCESS;
+	while (++i < g_data.p)
+	{
+		g_data.phil[i].pid = fork();
+		if (g_data.phil[i].pid < 0)
+			return (ft_error("Couldn't create a process"));
+		if (g_data.phil[i].pid == 0)
+			phi_life((void *)i);
+	}
+	waitpid(-1, &status, 0);
+	if (WIFEXITED(status))
+		ret = WEXITSTATUS(status);
+	return (ret);
+}
+
+int	main(int argc, char **argv)
+{
+	if (init(argc, argv) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	if (create_philo_processes() == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	if (create_monitor_threads() == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
---
+++ philo_three/srcs/start.c
@@ -0,0 +1,82 @@
+#include "philo_three.h"
+
+static int	parse_arguments(int argc, char **argv)
+{
+	int	i;
+
+	i = 0;
+	while (++i < argc)
+	{
+		if (ft_str_is_numeric(argv[i]) == 0)
+			return (EXIT_FAILURE);
+	}
+	g_data.p = ft_atoi(argv[1]);
+	g_data.time_to_die = ft_atoi(argv[2]);
+	g_data.time_to_eat = ft_atoi(argv[3]);
+	g_data.time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		g_data.must_eat = ft_atoi(argv[5]);
+	else
+		g_data.must_eat = 0;
+	return (EXIT_SUCCESS);
+}
+
+static int	validator(int argc, char **argv)
+{
+	if (argc < 5 || argc > 6)
+	{
+		printf("Wrong number of arguments: expected 4 or 5\n" \
+			   "1.number_of_philosophers 2.time_to_die 3.time_to_eat " \
+			   "4.time_to_sleep 5.number_of_times_each_philosopher_must_eat" \
+																"(optional)\n");
+		return (EXIT_FAILURE);
+	}
+	if (parse_arguments(argc, argv) == EXIT_FAILURE)
+	{
+		printf("Only numeric arguments are expected:\n"
+			   "1.number_of_philosophers 2.time_to_die 3.time_to_eat "
+			   "4.time_to_sleep 5.number_of_times_each_philosopher_must_eat" \
+																"(optional)\n");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
+
+static int	init_phils(void)
+{
+	int	i;
+
+	g_data.phil = (t_phil *)malloc(sizeof(t_phil) * g_data.p);
+	if (g_data.phil == NULL)
+		ft_error("Malloc returned error");
+	i = -1;
+	while (++i < g_data.p)
+	{
+		g_data.phil[i].last_meal = get_time();
+		g_data.phil[i].meals = 0;
+	}
+	return (EXIT_SUCCESS);
+}
+
+static void	init_semaphores(void)
+{
+	sem_unlink("/semaphore");
+	sem_unlink("/semaphore_write");
+	g_data.waiter = sem_open("/semaphore", O_CREAT, 0666, g_data.p);
+	if (g_data.waiter == SEM_FAILED)
+		ft_error("Couldn't open a semaphore");
+	g_data.write = sem_open("/semaphore_write", O_CREAT, 0666, 1);
+	if (g_data.write == SEM_FAILED)
+		ft_error("Couldn't open a semaphore");
+}
+
+int	init(int argc, char **argv)
+{
+	if (validator(argc, argv) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	if (init_phils() == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	init_semaphores();
+	g_data.start_time = get_time();
+	return (EXIT_SUCCESS);
+}
